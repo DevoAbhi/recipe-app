84ded21dfc2d71d24814f2d2389bfb340da8dc14
AbhiDroid <abhinabroy2001@gmail.com>
2020-12-25 23:15:44 +0530

    Removed resolver bug

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,17 +1,29 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve, RouterState, RouterStateSnapshot } from "@angular/router";
-import { DataStorageService } from "../shared/data-storage.service";
-import { Recipe } from "./recipe.model";
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterState,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Recipe } from './recipe.model';
+import { RecipeService } from './recipes.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class RecipesResolverService implements Resolve<Recipe[]> {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
-  constructor(private dataStorageService : DataStorageService) {}
-
-  resolve(route : ActivatedRouteSnapshot, state : RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    let recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
