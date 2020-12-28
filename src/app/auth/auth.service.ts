4890854a60cd8c820d85f556860774a0c93ca59b
AbhiDroid <abhinabroy2001@gmail.com>
2020-12-28 07:24:11 +0530

    Add user model for signup and login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   idToken: string;
@@ -16,6 +17,7 @@ export interface AuthResponseData {
   providedIn: 'root',
 })
 export class AuthService {
+  user = new Subject<User>();
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -28,7 +30,17 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleErrors));
+      .pipe(
+        catchError(this.handleErrors),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
 
   login(email: string, password: string) {
@@ -41,7 +53,29 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(catchError(this.handleErrors));
+      .pipe(
+        catchError(this.handleErrors),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userID: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    // create a new user
+    const user = new User(email, userID, token, expirationDate);
+    this.user.next(user);
   }
 
   private handleErrors(errorRes: HttpErrorResponse) {
@@ -57,7 +91,8 @@ export class AuthService {
         errorMessage = 'This email does not exist! Please sign up first!';
         break;
       case 'INVALID_PASSWORD':
-        errorMessage ='The password is invalid! Please enter the correct password!';
+        errorMessage =
+          'The password is invalid! Please enter the correct password!';
     }
     return throwError(errorMessage);
   }
